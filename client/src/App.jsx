4692098188148d94dd2a6dd57276802aba6a174a
Mikomijie <omijiemichael435@gmail.com>
2026-08-23 22:49:26 -0700

    Fix routing - landing page first, dashboard at /dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
 import Landing from './pages/Landing'
 import Captions from './pages/Captions'
 import Summary from './pages/Summary'
 import Lecturer from './pages/Lecturer'
+import Dashboard from './pages/Dashboard'
 import History from './pages/History'
 import Glossary from './pages/Glossary'
 
@@ -11,13 +11,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/join" element={<Landing />} />
-        <Route path="/captions" element={<Captions />} />
-        <Route path="/summary" element={<Summary />} />
-        <Route path="/lecturer" element={<Lecturer />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/glossary" element={<Glossary />} />
+        <Route path='/' element={<Landing />} />
+        <Route path='/captions' element={<Captions />} />
+        <Route path='/summary' element={<Summary />} />
+        <Route path='/lecturer' element={<Lecturer />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/history' element={<History />} />
+        <Route path='/glossary' element={<Glossary />} />
       </Routes>
     </BrowserRouter>
   )
